04aec104a00a79560bc0b1eb08aa8c324bf1c7a0
Kenneth Swanson <kswanson@1904labs.com>
2020-07-24 14:59:52 -0500

    Skeleton of test for imdb_data_pull

---
+++ test/dags/imdb_pull_unit_test.py
@@ -0,0 +1,47 @@
+import unittest
+import sqlite3
+from airflow.models import DagBag
+from airflow.hooks.sqlite_hook import SqliteHook
+from dags.imbd_pull import db_to_file
+import tempfile
+
+class TestImdbPull(unittest.TestCase):
+
+    def setUp(self):
+        self.dagbag = DagBag()
+        self.dag = self.dagbag.get_dag('imdb_data_pull')
+
+    def test_expected_endpoints(self):
+        tasks = self.dag.tasks
+
+        source_tasks = [x for x in tasks if len(x.upstream_list) == 0]
+        self.assertEqual(len(source_tasks), 1)
+
+        sink_tasks = [x for x in tasks if len(x.downstream_list) == 0]
+        self.assertEqual(len(sink_tasks), 2)
+
+    def test_db_to_file(self):
+        sqlite_conn = sqlite3.connect(':memory:')
+
+        sqlite_conn.execute('create table person(name text, age integer)')
+        data = [('Alice', 5),('Bob', 24), ('Carol', 33), ('Dave', 15)]
+        sqlite_conn.executemany('insert into person values (?, ?)', data)
+
+        class DBToFileHook(SqliteHook):
+            def get_conn(self):
+                return sqlite_conn
+
+        hook=DBToFileHook()
+        testfile=tempfile.NamedTemporaryFile()
+
+        db_to_file(hook, "select name from person where age > 18", testfile.name)
+
+        lines=testfile.readlines()
+
+        self.assertEqual(len(lines), 3)
+
+        
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestImdbPull)
+unittest.TextTestRunner(verbosity=2).run(suite)
